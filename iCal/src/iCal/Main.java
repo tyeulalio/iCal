a5a58a50072f46892c3aa0b903ea938a31aeaade
tyeulalio <tyeulalio@gmail.com>
2015-07-06 16:17:46 -1000

    Worked in main method; prompts to user

--- iCal/src/iCal/Main.java
+++ iCal/src/iCal/Main.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scan= new Scanner(System.in);
+		gatherData();	
 		
-		//creates an event
+	}
+	
+	public static void gatherData(){
+		Scanner scan= new Scanner(System.in);
 		Event event1 = new Event();
 		
 		//event's title
@@ -17,17 +20,21 @@ public class Main {
 		//event's description
 		System.out.print("Enter the event's description: ");
 		event1.description = scan.nextLine();
-		
+				
+		//event's start time
+		System.out.println("Enter event's start time in 24 hr format (e.g. 4 pm = 1600): ");
+		//event1.timeStart = ();
+				
 		//created date
-		
+				
+		//date modified
+				
 		
 		//Used to test code
 		System.out.println("Event is: " + event1.title);
 		System.out.println("Event description is: " + event1.description);
 		System.out.println("Date created is: " + event1.dateCreated);
 		
-		
-
 	}
 
 }
