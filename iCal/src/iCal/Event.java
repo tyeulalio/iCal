14c078095556b684a97159b4ed60413e25e6a397
josephyli <josephyli@gmail.com>
2015-07-06 14:37:06 -1000

    Designed structure

--- iCal/src/iCal/Event.java
+++ iCal/src/iCal/Event.java
@@ -2,8 +2,25 @@ package iCal;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Event {
+	String title;
+	String description;
+	int dateCreated; // CREATED
+	int dateLastModified; // LAST-MODIFIED
+	int timeStart; // DTSTART
+	int timeEnd; // DTEND
+	String uid;
+	String classType;
+	int location;
 	
+	public static void getTime()
+	{
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+	    System.out.println( sdf.format(cal.getTime()) );
+	}
 	
 }
