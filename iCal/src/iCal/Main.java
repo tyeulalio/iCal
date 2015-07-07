5066cc380a40ab6e3d195e1616570c2be8833f7c
tyeulalio <tyeulalio@gmail.com>
2015-07-06 15:41:26 -1000

    Working in main method; made new event

--- iCal/src/iCal/Main.java
+++ iCal/src/iCal/Main.java
@@ -14,9 +14,18 @@ public class Main {
 		System.out.print("Enter the event's title: ");
 		event1.title =  scan.nextLine();
 		
-		//event's 
+		//event's description
+		System.out.print("Enter the event's description: ");
+		event1.description = scan.nextLine();
 		
+		//created date
+		
+		
+		//Used to test code
 		System.out.println("Event is: " + event1.title);
+		System.out.println("Event description is: " + event1.description);
+		System.out.println("Date created is: " + event1.dateCreated);
+		
 		
 
 	}
