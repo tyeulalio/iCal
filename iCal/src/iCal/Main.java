d95ed391bc202b66bd9fc5f8a6c159a668d6e88e
tyeulalio <tyeulalio@gmail.com>
2015-07-06 15:30:28 -1000

    Worked in main method

--- iCal/src/iCal/Main.java
+++ iCal/src/iCal/Main.java
@@ -1,11 +1,23 @@
 package iCal;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Our iCal project... yeay!");
+		Scanner scan= new Scanner(System.in);
 		
-		System.out.print("Test2");
+		//creates an event
+		Event event1 = new Event();
+		
+		//event's title
+		System.out.print("Enter the event's title: ");
+		event1.title =  scan.nextLine();
+		
+		//event's 
+		
+		System.out.println("Event is: " + event1.title);
+		
 
 	}
 
